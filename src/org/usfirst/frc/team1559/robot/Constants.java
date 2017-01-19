61188a27196105f27561781ff46f75d00cc13431
johndunaske <j_dunaske@yahoo.com>
2017-01-19 18:44:12 -0500

    JK its mecanum
    wreck'd

--- src/org/usfirst/frc/team1559/robot/Constants.java
+++ src/org/usfirst/frc/team1559/robot/Constants.java
@@ -1,5 +1,21 @@
 package org.usfirst.frc.team1559.robot;
 
 public class Constants {
-
+	// again, thou shan't use magical digits
+	
+	//Talon stuffs
+	public static final double P = 0.3;
+	public static final double I = 0;
+	public static final double D = 0.05;
+	public static final double F = 0.32;
+	public static final float NOMIAL_OUTPUT_VOLTAGE = +0.0f;
+	public static final float PEAK_OUTPUT_VOLTAGE = +12.0f;
+	public static final float NEGATIVE_NOMIAL_OUTPUT_VOLTAGE = -0.0f;
+	public static final float NEGATIVE_PEAK_OUTPUT_VOLTAGE = -0.0f;
+	public static final double RPM_CONVERSION = (300)/(4096*.1029);
+	
+	//Shifting
+	public static final double SHIFT_UP_SPEED = 4.6; // m/s
+	public static final double SHIFT_DOWN_SPEED = 4.4; // m/s
+	public static final double LOW_SPEED_MULTIPLIER = 1.0 ; //Reccommended 2.2, Chris didn't like it....
 }
--- src/org/usfirst/frc/team1559/robot/Wiring.java
+++ src/org/usfirst/frc/team1559/robot/Wiring.java
@@ -1,5 +1,26 @@
 package org.usfirst.frc.team1559.robot;
 
 public class Wiring {
-
+	
+	// thou shan't use magical digits
+	
+	//Talon stuffs
+	public static final int FL_SRX = 1;
+	public static final int FR_SRX = 2;
+	public static final int RL_SRX = 3;
+	public static final int RR_SRX = 4;
+	
+	// Dropper
+	public static final int SHIFT_1 = 3;
+	public static final int SHIFT_2 = 4;
+	
+	// Joysticks
+	public static final int JOYSTICK0 = 0;
+	public static final int JOYSTICK1 = 1;
+	public static final int JOYSTICK2 = 2;
+	public static final int JOYSTICK3 = 3;
+	
+	// gatherer/shooter controls
+	public static final int BTN_GATHER = 1;
+	public static final int BTN_SHOOT = 2;
 }
