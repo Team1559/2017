c5945670146a56a1a4469408c377011970712c0d
Lucariax <lucariax@hotmail.com>
2017-01-19 19:58:48 -0500

    dad changed it
    :100:

--- src/org/usfirst/frc/team1559/robot/Shooter.java
+++ src/org/usfirst/frc/team1559/robot/Shooter.java
@@ -1,69 +1,73 @@
 package org.usfirst.frc.team1559.robot;
 
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.Joystick;
+import com.ctre.CANTalon;
+import com.ctre.CANTalon.FeedbackDevice;
+import com.ctre.CANTalon.TalonControlMode;
+
 import edu.wpi.first.wpilibj.Servo;
-import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.Talon;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Shooter {
 	// The Shooter is a Fly Wheel.
 	// This is where all the variables are instantiated.
-	boolean LOOP_STATUS, SERVO_BOOLEAN, GATE_STATUS;
-	// LOOP_SATUS - Boolean for the speed of the speed controller.
+	boolean SERVO_BOOLEAN, GATE_STATUS;
 	// SERVO_BOOLEAN - Sets the angle of the Servo, based on speed.
 	// GATE_STATUS - If the gate is closed, no balls are allowed in, and if open, vice versa.
 	Servo BALL_OPENER;
 	// The instantiation of the servo.
-	Talon SRX;
-	Encoder SRX_Checker;
+	CANTalon SRX;
 	// The Encoder checks the rate of fire for the talon.
 	// The talon allows the Robot to control the rate of fire.
-	// The ERROR_LIGHT alerts the Team if the code malfunctions.
+	// The Joystick allows the pilots to control the fire of the balls. (Will change)
 	int i;
 	// For all for loops.
 	public void ShooterInit() {
 		// All the variables are defined here.
 		BALL_OPENER = new Servo(1); // Will change for actual Robot.
-		SRX = new Talon(1); // Will change for the actual Robot.
-		LOOP_STATUS = false; // Starts out defined as false, ball rate not limited.
+		SRX = new CANTalon(1);// Will change for the actual Robot.
 		SERVO_BOOLEAN = true; // Starts out defined as true, the servo is open.
 		GATE_STATUS = true; // The gate starts out as open.
 		i = 0; // To be used with the for loop to limit the ball rate.
+		SRX.setFeedbackDevice(FeedbackDevice.QuadEncoder);
+		SRX.configEncoderCodesPerRev(4096);
+		SRX.changeControlMode(TalonControlMode.Speed);
+		SRX.configNominalOutputVoltage(+0.0f, -0.0f);
+		SRX.configPeakOutputVoltage(+12.0f, 0.0f);
+		SRX.setProfile(0);
+		SRX.setP(0);
+		SRX.setI(0);
+		SRX.setD(0);
+		SRX.setF(0);
+		
 	}
 	public void ShooterGate() {
 		// Controls the functions of the gate.
-		if(!LOOP_STATUS && GATE_STATUS) {
+		if(GATE_STATUS) {
 			SERVO_BOOLEAN = true; // The rate of fire is not limited.
 			BALL_OPENER.set(0.0); // The angle will most definitely change. Also, closes the way for balls to get in.
 		}
-		else if(LOOP_STATUS && GATE_STATUS) {
+		else if(GATE_STATUS) {
 			for(i = 0; i <= 101; i++) { // 1 integer value is equal to about 20ms.
 				SERVO_BOOLEAN = false; // The rate of fire is limited
 				if(i <= 50) // tick, tick.
-					BALL_OPENER.set(0.0); // Opens the way for balls to get in.
+					BALL_OPENER.setAngle(0.0); // Opens the way for balls to get in.
 				else if(i > 50 && i <= 100) // tick, tick.
-					BALL_OPENER.set(90.00); // Closes the way for balls to get in.
+					BALL_OPENER.setAngle(90.00); // Closes the way for balls to get in.
 				else if(i == 101) // default scenario
 					i = 0; // the for loop resets.
 			}
 		}
-		else if(LOOP_STATUS && !GATE_STATUS) {
+		else if(!GATE_STATUS) {
 			for(i = 0; i < 200; i++) { // 1 integer value is equal to about 20ms.
 				SERVO_BOOLEAN = false; // The rate of fire is limited
 			}
 		}
 		else {
 			SERVO_BOOLEAN = false; // The rate of fire is limited
-			BALL_OPENER.set(90.0); // Closes the way for balls to get in.
+			BALL_OPENER.setAngle(90.0); // Closes the way for balls to get in.
 		}
 	}
-	public void ShooterLaunch() {
-		// Controls functions of the balls at launch.
-	}
-	public void SpeedControl() {
-		
-		// Allows the rate of balls to be controlled, so the Robot is not overloaded and the balls are not continually.
+	
+	public void ShooterMotor(int rpm){
+		SRX.set(rpm*300/4096);
 	}
 }
