6232054c2fe20bceb8f15c3978e5c52e2d15be3f
NateBacon <bakernathan1299@gmail.com>
2017-02-08 19:20:23 -0500

    added a getIMUAngle method written in 10 mins
    this most likely doesn't work

--- src/org/usfirst/frc/team1559/robot/Robot.java
+++ src/org/usfirst/frc/team1559/robot/Robot.java
@@ -1,6 +1,8 @@
 
 package org.usfirst.frc.team1559.robot;
 
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.I2C.Port;
 import edu.wpi.first.wpilibj.IterativeRobot;
 
 public class Robot extends IterativeRobot {
@@ -12,6 +14,7 @@ public class Robot extends IterativeRobot {
 	//Climber climber; //Creates the Climber
 	//Diagnostics diagnostics; //Create the diagnostics tool
 	OperatorInterface oi; //Create the OI
+	I2C imu;//QUICK AND DIRTY -NATE
 	
 	
 	@Override
@@ -23,6 +26,13 @@ public class Robot extends IterativeRobot {
 		oi = OperatorInterface.getInstance(); //Instantiate the OI
 		//diagnostics = new Diagnostics();
 		//climber = Climber.getInstance();
+		imu = new I2C(Port.kOnboard, 0x28);//QUICK AND DIRTY -NATE
+		setIMU();//QUICK AND DIRTY -NATE
+		
+	}
+	
+	public void setIMU(){//QUICK AND DIRTY -NATE
+		imu.write(0x3D, 0x03);
 	}
 
 	@Override
@@ -40,8 +50,18 @@ public class Robot extends IterativeRobot {
 		driveTrain.drop(false); //Make sure we are in traction mode
 	}
 
+	public double getIMUAngle(){//QUICK AND DIRTY -NATE
+		double angle = 0.0;
+		byte[] gyroZLSB = new byte[1];
+		byte[] gyroZMSB = new byte[1];
+		imu.read(0x18, 1, gyroZLSB);
+		imu.read(0x19, 1, gyroZMSB);
+		angle = (double)((gyroZMSB[0] << 8) | gyroZLSB[0]);
+		return angle;
+	}
 	@Override
 	public void teleopPeriodic() {
+		//JOHN, GRAB AN ANGLE FROM THE GYRO WITH THE getIMUAngle METHOD. THIS WS WRITTEN IN TEN MINUTES SO I WOULD TEST THE METHOD OUT FIRST -NATE
 		oi.updateButtons(); //update controller values
 		//gearGatherer.update(oi.getDriverStick().getRawButton(Constants.GEAR_GATHERER)); //Update gear gatherer
 		
