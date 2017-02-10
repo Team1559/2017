f59961c933d9bf0b06f535e13c6001a3c9b4339b
johndunaske <j_dunaske@yahoo.com>
2017-02-09 20:43:00 -0500

    yay copy and paste
    hour wasted

--- src/org/usfirst/frc/team1559/robot/Constants.java
+++ src/org/usfirst/frc/team1559/robot/Constants.java
@@ -6,27 +6,29 @@ public class Constants {
 	// Drive Train
 	public static final double MAX_DRIVE_SPEED = .685; //The maximum speed the mecanum wheels can go
 	public static final int DROP_BUTTON = 3; //Button that drops/picks up the mecanum wheels
+	public static final int PRIMARY_BUFFER_LENGTH = 8;
+	public static final int SECONDARY_BUFFER_LENGTH = 4;
 	
 	//Talon stuffs
-	public static final double Pd = 0.3; //(This may change) The proportional value for the PIDF loop (Mecanum)
+	public static final double Pd = 0.1; //(This may change) The proportional value for the PIDF loop (Mecanum)
 	public static final double Id = 0; //(This may change) The integral value for the PIDF loop (Mecanum)
-	public static final double Dd = 0.05; //(This may change) The derivative  value for the PIDF loop (Mecanum)
+	public static final double Dd = 0.0; //(This may change) The derivative  value for the PIDF loop (Mecanum)
 	public static final double Fd = 0.32; //(This may change) The feed-forward value for the PIDF loop (Mecanum)
 	
-	public static final double Ps = 0.3; //(This may change) The proportional value for the PIDF loop (Mecanum)
-	public static final double Is = 0; //(This may change) The integral value for the PIDF loop (Mecanum)
-	public static final double Ds = 0.05; //(This may change) The derivative  value for the PIDF loop (Mecanum)
-	public static final double Fs = 0.32; //(This may change) The feed-forward value for the PIDF loop (Mecanum)
+	public static final double Ps = 0.3; //(This may change) The proportional value for the PIDF loop (shooter)
+	public static final double Is = 0; //(This may change) The integral value for the PIDF loop (shooter)
+	public static final double Ds = 0.0; //(This may change) The derivative  value for the PIDF loop (shooter)
+	public static final double Fs = 0.32; //(This may change) The feed-forward value for the PIDF loop (shooter)
 	
 	public static final int ENCODER_CODES_PER_REV = 4096;// The number of times an encoder will move in one wheel revolution
 	
 	public static final float NOMIAL_OUTPUT_VOLTAGE = +0.0f; //The forward nomial output voltage for the talons
 	public static final float PEAK_OUTPUT_VOLTAGE = +12.0f; //The reverse nomial output voltage for the talons
 	public static final float NEGATIVE_NOMIAL_OUTPUT_VOLTAGE = -0.0f; //The forward peak output voltage for the talons
-	public static final float NEGATIVE_PEAK_OUTPUT_VOLTAGE = -0.0f; //The reverse peak output voltage for the talons
+	public static final float NEGATIVE_PEAK_OUTPUT_VOLTAGE = -12.0f; //The reverse peak output voltage for the talons
 	public static final int PROFILE = 0; //The profile for the talons
 	public static final int SHOOTER_TALON_PROFILE = 0; //The TalonSRX profile for shooting
-	public static final double RPM_CONVERSION = (300)/(4096*.1029); //This will change
+	public static final double RPM_CONVERSION = (300)/(4096); //This will change
 	public static final double FIRE_DELAY = 1.0/3;
 	public static final double CLOSE_DELAY = 1.0/8;
 	public static final int OPEN_VAL = 90;
