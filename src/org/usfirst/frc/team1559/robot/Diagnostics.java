d80bc33eef64f4e1c938718590ade94211297621
HorihanA <andrew@horihans.com>
2017-02-11 12:26:16 -0500

    Diagnostics tool
    You have to make a Diagnostics object in your class that is testing
    stuff. The methods to output data to the SmartDashboard are just the
    names of the device you're testing. For example, to output a certain
    servo's data, call the Servo method. You'll have to pass in the servo
    and what you want it to be called on the dashboard. Every time you test
    a new thing it makes a new label thing in the SmartDashboard. Let me
    know if I have to add other methods to test other classes, right now
    there's only like 4. Afterwords I'll edit the SmartDashboard on the
    laptop we're using at competitions so it looks all nice, because when
    you create a new label by calling methods it just puts it in the upper
    left corner.

--- src/org/usfirst/frc/team1559/robot/Diagnostics.java
+++ src/org/usfirst/frc/team1559/robot/Diagnostics.java
@@ -1,6 +1,6 @@
 package org.usfirst.frc.team1559.robot;
-//                        :-)
 
+//                   : )
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
@@ -13,7 +13,7 @@ public class Diagnostics {
 
 	public SmartDashboard x;
 	
-	public Diagnostics() //the constructor
+	public Diagnostics() //the constructor -- Do not pass in the SmartDashboard from other classes
 	{
 		x = new SmartDashboard();
 	}
@@ -21,64 +21,35 @@ public class Diagnostics {
 //       --Call these methods to test various things--
 	
 	//Encoders
-	public void encoderData(Encoder e) //call this to output encoder data (distance)
+	public void Encoder(Encoder e, String name) //call this to output encoder data (distance)
 	{
 		String key = "Encoder Distance: ";
 		SmartDashboard.putString(key, (e.getDistance() + ""));				
 	}
 	//-----------------------------------------------------------------------------------------
 	//Servos
-	public void servoData(Servo s) //call to output servo data (angle)
+	public void Servo(Servo s, String name) //call to output servo data (angle)
 	{
 		String key = "Servo Angle: ";
 		SmartDashboard.putString(key, (s.getAngle() + ""));				
 	}
-	
-	public void testServoData(Servo s) //will tell you if the servo is working
-	{
-			double num1 = s.getAngle(); //moves the servo and sees if it actually moves
-			double num2;
-			if(num1 == 0)
-			{
-				s.setAngle(5);
-				num2 = s.getAngle();
-			}
-			else
-			{
-				s.setAngle(0);
-				num2 = s.getAngle();
-			}
-			boolean check = (num1 != num2);
-			String key = "Servo is working: ";
-			SmartDashboard.putString(key, (check + ""));	
-	}
 	//-----------------------------------------------------------------------------------------
 	//Gyros
-	public void gyroData(Gyro g) //outputs gyro data (angle)
+	public void Gyro(Gyro g, String name) //outputs gyro data (angle)
 	{
 		String key = "Gyro Angle: ";
 		SmartDashboard.putString(key, (g.getAngle() + ""));				
 	}
 	//-----------------------------------------------------------------------------------------
 	//Solenoids
-	public void solenoidData(Solenoid s) //outputs solenoid data (on or off)
+	public void Solenoid(Solenoid s, String name) //outputs solenoid data (on or off)
 	{
-		String key = "Solenoid Status: ";
+		String key = name + " Status: ";
 		SmartDashboard.putString(key, (s.get() + ""));				
-	}
-	
-	public void testSolenoidData(Solenoid s) //tests the solenoid
-	{
-		boolean state1 = s.get(); //turns on the solenoid and sees if it actually turned on
-		s.set(!state1);
-		boolean state2 = s.get();
-		s.set(state1);
-		String key = "Solenoid is working: ";
-		SmartDashboard.putString(key, ((state1 != state2) + ""));	
-	}
+	}	
 	//-----------------------------------------------------------------------------------------
 	//Digital inputs
-	public void digitalInputData(DigitalInput d)  //outputs the value from the digital input channel (true or false)
+	public void DigitalInput(DigitalInput d, String name)  //outputs the value from the digital input channel (true or false)
 	{
 		String key = "DigitalInput Status: ";
 		SmartDashboard.putString(key, (d.get() + ""));				
