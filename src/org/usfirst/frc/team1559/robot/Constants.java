92b90050a115a4c4e0cb66c9f8fbeedb7845af3b
RyanLuu <rl65536@gmail.com>
2017-02-18 16:58:48 -0500

    Deleted unnecessary constants

--- src/org/usfirst/frc/team1559/robot/Constants.java
+++ src/org/usfirst/frc/team1559/robot/Constants.java
@@ -2,12 +2,6 @@ package org.usfirst.frc.team1559.robot;
 
 public class Constants {
 	// again, thou shan't use magical digits
-
-	// Drive Train
-	public static final double MAX_DRIVE_SPEED = .685; //The maximum speed the mecanum wheels can go
-	public static final int DROP_BUTTON = 3; //Button that drops/picks up the mecanum wheels
-	public static final int PRIMARY_BUFFER_LENGTH = 8;
-	public static final int SECONDARY_BUFFER_LENGTH = 4;
 	
 	//Talon stuffs
 	public static final double Pd = -200000; //(This may change) The proportional value for the PIDF loop (Mecanum)
@@ -22,27 +16,12 @@ public class Constants {
 	public static final double Fs = 0.0422; //(This may change) The feed-forward value for the PIDF loop (shooter)
 	
 	public static final int ENCODER_CODES_PER_REV = 4096;// The number of times an encoder will move in one wheel revolution
-	public static final int ENCODER_CODES_PER_REV_SHOOTER = 4096;
 	
 	public static final float NOMIAL_OUTPUT_VOLTAGE = +0.0f; //The forward nomial output voltage for the talons
 	public static final float PEAK_OUTPUT_VOLTAGE = +12.0f; //The reverse nomial output voltage for the talons
 	public static final float NEGATIVE_NOMIAL_OUTPUT_VOLTAGE = -0.0f; //The forward peak output voltage for the talons
 	public static final float NEGATIVE_PEAK_OUTPUT_VOLTAGE = -12.0f; //The reverse peak output voltage for the talons
 	public static final int PROFILE = 0; //The profile for the talons
-	public static final int SHOOTER_TALON_PROFILE = 0; //The TalonSRX profile for shooting
 	public static final int SHOOTER_RPM = 250;//Need to check, also could change based on distance
 	public static final double RPM_CONVERSION = (300)/(4096); //This will change
-	
-	//Shifting
-	public static final double SHIFT_UP_SPEED = 4.6; // m/s
-	public static final double SHIFT_DOWN_SPEED = 4.4; // m/s
-	public static final double LOW_SPEED_MULTIPLIER = 1.0 ; //Recommended 2.2, According to Cody...
-
-	//Driver Controls
-	public static final int SHIFTER = 4;
-	public static final double SHOOTER_TOLERANCE = 0.2;
-	public static final int SHOOTER_AXIS = 3;
-	public static final int GEAR_GATHERER = 1;
-	public static final int BALL_GATHERER = 6;
-	public static final int CLIMBER = 2;
 }
