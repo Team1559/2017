b408c38f0bcc3f12cb6a6867297529cf2c3af28f
Lucariax <lucariax@hotmail.com>
2017-02-18 21:55:34 -0500

    Modified... again

--- src/org/usfirst/frc/team1559/robot/Shooter.java
+++ src/org/usfirst/frc/team1559/robot/Shooter.java
@@ -12,15 +12,19 @@ import edu.wpi.first.wpilibj.Talon;
 public class Shooter extends Subsystem {
 
 	private static Shooter instance;
+
 	public static Shooter getInstance() { // Gets the instance of the Shooter.
 		if (instance == null) {
 			instance = new Shooter();
 		}
 		return instance;
 	}
+	
 	// The Shooter is a Fly Wheel Shooter.
 	// This is where most of the variables are instantiated.
-	CANTalon shooterTalon; Talon shooterFeeder; // The motor that is used to fire them balls & The motor used to feed the balls.
+	CANTalon shooterTalon;
+	Talon shooterFeeder; // The motor that is used to fire them balls & The motor used to feed the balls.
+
 	public Shooter() { // Used to define the CANTalon and other variables.
 		super("shooter");
 		// All the variables are defined here.
@@ -30,7 +34,7 @@ public class Shooter extends Subsystem {
 		shooterTalon.setFeedbackDevice(FeedbackDevice.QuadEncoder); // Sets the feedback device to a Quad Encoder.
 		shooterTalon.configEncoderCodesPerRev(Constants.ENCODER_CODES_PER_REV); // Encoder codes per Revolution.
 		shooterTalon.changeControlMode(TalonControlMode.Speed); // Sets the control mode to Speed.
-		shooterTalon.configNominalOutputVoltage(Constants.NOMIAL_OUTPUT_VOLTAGE, Constants.NEGATIVE_PEAK_OUTPUT_VOLTAGE); // Sets the output voltages.
+		shooterTalon.configNominalOutputVoltage(Constants.NOMIAL_OUTPUT_VOLTAGE,Constants.NEGATIVE_PEAK_OUTPUT_VOLTAGE); // Sets the output voltages.
 		shooterTalon.configPeakOutputVoltage(Constants.PEAK_OUTPUT_VOLTAGE, Constants.NOMIAL_OUTPUT_VOLTAGE); // Sets the peak voltage and the minimum voltage output.
 		shooterTalon.setProfile(Constants.PROFILE); // Sets the profile of the CANTalon.
 		shooterTalon.setP(Constants.Ps); // Ps = 0.2
@@ -38,21 +42,27 @@ public class Shooter extends Subsystem {
 		shooterTalon.setD(Constants.Ds); // Ds = 0
 		shooterTalon.setF(Constants.Fs); // Fs = 0.0422
 	}
+
 	public void setShooter(double rpm) { // Starts up the motor so that them balls can be fired.
 		shooterTalon.set(rpm); // Motor is started up.
 	}
+
 	public void setFeeder(double feedSpeed) {
 		shooterFeeder.set(feedSpeed);
 	}
-	public void fire(boolean fire) { // Used to control the fire rate of the balls
+
+	public void fire(boolean fire) { // Used to control the fire rate of the
+										// balls
 		if (fire) {
-			setShooter(Constants.SHOOTER_SPEED); //TODO: Set to the right speed
-			setFeeder(Constants.FEEDER_SPEED); //TODO: Find the perfect speed.
+			setShooter(Constants.SHOOTER_SPEED); // TODO: Set to the right speed
+			setFeeder(Constants.FEEDER_SPEED); // TODO: Find the perfect speed.
 		} else {
-			setShooter(0); //Sets the fire rate to 0, stopping the shooter.
+			setShooter(0); // Sets the fire rate to 0, stopping the shooter.
+			setFeeder(0); // Sets the feed rate to 0, stopping it.
 		}
 	}
+
 	public void getState(State s) { // The method that will set the state of s
-		s.put("shooter-velocity", shooterTalon.getEncVelocity()*Constants.RPM_CONVERSION); // TODO: fix
+		s.put("shooter-velocity", shooterTalon.getEncVelocity() * Constants.RPM_CONVERSION); // TODO: Fix this.
 	}
 }
