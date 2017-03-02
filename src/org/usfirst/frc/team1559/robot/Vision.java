0e93c0cd3f07b84a61e0abde3602491dc5fcd446
DrWillPhd <wdmerges@gmail.com>
2017-03-02 18:37:53 -0500

    updated vision class
    distance

--- src/org/usfirst/frc/team1559/robot/Vision.java
+++ src/org/usfirst/frc/team1559/robot/Vision.java
@@ -4,6 +4,7 @@ public class Vision {
 
 	UDPClient client;
 	private double angle;
+	private double distance;
 
 	private Vision() {
 		client = new UDPClient();
@@ -11,12 +12,20 @@ public class Vision {
 
 	public void update() {
 		try {
-			double temp = Double.parseDouble(client.get());
+			double temp = Double.parseDouble(client.getAngle());
 			if (temp != -1000) {
 				angle = temp;
 			}
 		} catch (NumberFormatException | NullPointerException e) {
 			System.err.println("nothing happening here");
+		}
+		try {
+			double tempd = Double.parseDouble(client.getDistance());
+			if (tempd != -1000) {
+				distance = tempd;
+			}
+		} catch (NumberFormatException | NullPointerException e) {
+			System.err.println("rats for lunch");
 		}
 	}
 
@@ -32,5 +41,9 @@ public class Vision {
 
 	public double getAngle() {
 		return angle;
+	}
+	
+	public double getDistance(){
+		return distance;
 	}
 }
